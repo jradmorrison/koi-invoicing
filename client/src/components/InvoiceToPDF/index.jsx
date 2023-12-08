48ae0c29922a65c28b0fc94873f9ddbd86273c23
Roesn <Bubwub04@icloud.com>
2023-12-08 14:20:17 -0500

    styled invoice page and functrionality to download invoice

--- client/src/components/InvoiceToPDF/index.jsx
+++ client/src/components/InvoiceToPDF/index.jsx
@@ -3,75 +3,76 @@ import { PDFExport, savePDF } from "@progress/kendo-react-pdf"
 import { useRef } from "react"
 
 // invoice component
-const Invoice = ({ invoice: { businessID, clientEmail, totalBalance, status, dateDue, serviceProvided, createdOn } }) => {
+const InvoiceToPDF = ({ invoice: { invoiceID, businessID, clientEmail, totalBalance, status, dateDue, serviceProvided, createdOn } }) => {
     // for exporting to pdf
     const pdfExportComponent = useRef(null);
     const handleExportComponent = (event) => {
         pdfExportComponent.current.save()
     }
 
-    // TODO: make qurery to db to get business name from id
-    const businessName = ""
-
     // TODO: handle stripe checkout
     const handleMakePayment = (event) => {
         event.preventDefault();
     }
 
+    // TODO: make qurery to db to get business name from id
+    const businessName = ""
+
     return ( // return invoice page with invoice and 2 buttons
         <div style={{ display: "flex" }}>
-
-            <div style={{ paddingRight: "2rem" }}>
+            <div style={{ paddingRight: "9rem" }}>
                 <PDFExport ref={pdfExportComponent} paperSize="A4">
                     <div style={{ backgroundColor: "#FFFFFF", color: "#000000", padding: "1rem", width: "35rem", height: "50rem" }}>
                         <div style={{ padding: "1rem" }}>
                             <div style={{ display: "flex", justifyContent: "start", alignItems: "center", height: "5rem" }}>
                                 <img src="" style={{ aspectRatio: "1", width: "4rem", paddingRight: "1rem" }}></img>
-                                <h1 style={{ color: "black", fontWeight: "bolder", fontSize: "1.4rem" }}>Business Name</h1>
+                                <h1 style={{ color: "black", fontWeight: "bolder", fontSize: "1.6rem" }}>Business Name</h1>
                             </div>
                             <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingTop: "2rem" }}>
                                 <div style={{ display: "flex", flexDirection: "column" }}>
                                     <h1 style={{ paddingBottom: ".2rem", paddingTop: "2rem", fontWeight: "bold", fontSize: "1.2rem" }}>From</h1>
-                                    <h3>Business Name</h3>
+                                    <h3 style={{ paddingBottom: ".2rem", fontSize: "1rem"}}>{businessID}</h3>
                                 </div>
                                 <div style={{ display: "flex", flexDirection: "column" }}>
                                     <h1 style={{ paddingBottom: ".2rem", paddingTop: "2rem", fontWeight: "bold", fontSize: "1.2rem" }}>Invoice Date</h1>
-                                    <h3>Creation Date</h3>
+                                    <h3 style={{ paddingBottom: ".2rem", fontSize: "1rem"}}>{createdOn}</h3>
                                 </div>
                             </div>
                             <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
                                 <div style={{ display: "flex", flexDirection: "column" }}>
                                     <h1 style={{ paddingBottom: ".2rem", paddingTop: "2rem", fontWeight: "bold", fontSize: "1.2rem" }}>To</h1>
-                                    <h3>Cllient Email</h3>
+                                    <h3 style={{ paddingBottom: ".2rem", fontSize: "1rem"}}>{clientEmail}</h3>
                                 </div>
                                 <div style={{ display: "flex", flexDirection: "column" }}>
                                     <h1 style={{ paddingBottom: ".2rem", paddingTop: "2rem", fontWeight: "bold", fontSize: "1.2rem" }}>Payment Due</h1>
-                                    <h3>Due Date</h3>
+                                    <h3 style={{ paddingBottom: ".2rem", fontSize: "1rem"}}>{dateDue}</h3>
                                 </div>
                             </div>
-                            <div style={{ paddingTop: "5rem", paddingBottom: "9rem" }}>
-                                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", backgroundColor: "#808080", padding: ".5rem" }}>
-                                    <h2>Description</h2>
-                                    <h2>Price</h2>
+                            <div style={{ paddingTop: "5rem", paddingBottom: "5rem" }}>
+                                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center",backgroundColor: "#808080", padding: ".3rem" }}>
+                                    <h2 style={{fontSize: "1rem"}}>Description</h2>
+                                    <h2 style={{fontSize: "1rem"}}>Price</h2>
                                 </div>
-                                <div>Service Provided</div>
+                                <div>{serviceProvided}</div>
                             </div>
 
                             <div style={{ paddingLeft: "12rem", width: "20rem", paddingBottom: ".7rem" }}>
-                                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingBottom: ".5rem" }}>
+                                <div style={{ display: "flex", flexDirection: "row",
+                                width: "18rem", justifyContent: "space-between", alignItems: "center", paddingBottom: ".5rem" }}>
                                     <p>Total</p>
                                     <p>Service Price</p>
                                 </div>
 
-                                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center",
+                                width: "18rem" }}>
                                     <p>Taxes</p>
                                     <p>Tax Amount</p>
                                 </div>
                             </div>
 
-                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingLeft: "15rem", paddingTop: ".7rem", width: "17rem", fontSize: "1.2rem", borderTop: "1px solid #00000", boxSizing: "content-box" }}>
-                                <h3 style={{ paddingRight: "5rem" }}>Total Due</h3>
-                                <h3>Total Balance</h3>
+                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingLeft: "15rem", paddingTop: ".2rem", width: "17rem", fontSize: "1.2rem", borderTop: "1px solid #00000", boxSizing: "content-box" }}>
+                                <h3 style={{ paddingRight: "5rem", fontSize: "1.2rem"}}>Total Due</h3>
+                                <h3 style={{paddingRight: "2rem",fontSize: "1.2rem"}}>{totalBalance}</h3>
                             </div>
                         </div>
                         <footer style={{ display: "flex", justifyContent: "start", paddingTop: "2rem", color: "black", height: "2rem", fontSize: "1rem" }}>
@@ -81,15 +82,22 @@ const Invoice = ({ invoice: { businessID, clientEmail, totalBalance, status, dat
                     </div>
                 </PDFExport >
             </div>
-
-            <div>
-                <button onClick={handleExportComponent} style={{ padding: "1rem", borderRadius: "1rem", backgroundColor: "#FF0000" }}>Export to PDF</button>
-                <button onClick={handleMakePayment} style={{ padding: "1rem", borderRadius: "1rem", backgroundColor: "#0000FF" }}>Make Payment</button>
+            <div style={{display: "flex", flexDirection: "column", paddingTop: "16rem"}}>
+                <div style={{display:"flex", flexDirection: "column", color: "#D9D9D9"}}>
+                    <h1 style={{fontSize: "3rem", padding: "0"}}>Invoice - {businessID}</h1>
+                    <p style={{fontSize: "1.5rem", padding: "0", margin: "0"}}>Date Billed: {createdOn}</p>
+                    <p style={{fontSize: "1.5rem", padding: "0", marginTop: "0"}}>Ref ID: {invoiceID}</p>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "#D9D9D9", padding: "1rem", borderRadius: ".3rem", minWidth: "20rem", minHeight: "4rem", marginBottom: "3rem"}}>
+                    <p>Due {dateDue}</p>
+                    <h4>${totalBalance}</h4>
+                </div>
+                <button onClick={handleExportComponent} style={{ padding: ".5rem", borderRadius: "1rem", backgroundColor: "#FDC447", marginBottom: "1rem", textAlign: "center", color: "#010144"}}>Export to PDF</button>
+                <button onClick={handleMakePayment} style={{ padding: ".5rem", borderRadius: "1rem", backgroundColor: "#FDC447", color: "#010144"}}>Make Payment</button>
             </div>
-
         </div>
     );
 }
 
 // export Invoice component
-export default Invoice;
+export default InvoiceToPDF;
--- client/src/pages/Invoice.jsx
+++ client/src/pages/Invoice.jsx
@@ -1,39 +1,48 @@
 import { Navigate } from 'react-router-dom';
 import Auth from '../utils/auth';
+import Button from '@mui/material/Button';
 
 import { useQuery } from '@apollo/client';
 // import {  } from '../utils/queries';
 import Header from '../components/Header'
 
+import InvoiceToPDF from '../components/InvoiceToPDF'
 
+const demoInvoice = {
+  invoiceID: "92842",
+  businessID: "123",
+  clientEmail: "democlient@email.com",
+  totalBalance: "100",
+  status: "Overdue",
+  dateDue: "12/04/2023",
+  serviceProvided: "Website Development",
+  createdOn: "11/26/2023"
+}
+
+// invoice page component
 const Invoice = () => {
-  // const { loading, data } = useQuery(QUERY_PROFILES);
-  // const profiles = data?.profiles || [];
 
+  // make sure business logged in
   if (!Auth.loggedIn()) {
     return <Navigate to="/login" />;
   }
 
+  // return 
   return (
-    <main>
+    <div>
       <Header />
-      <div><Button variant="text">Back to Dashboard</Button></div>
+
       <div>
-        <p>Preview?</p>
+        <Button style={{margin: "1rem", color: "#D9D9D9"}} variant="text">Back to Dashboard</Button>
       </div>
-      <div>
-        <h1>Invoice Name</h1>
-        <h4>Date Billed xx/xx/xxxx</h4>
-        <h4>Invoice ID: XXXXX </h4>
-        <div>
-            <h3>Due xx/xx/xxxx </h3>
-            <h2>$$$$</h2>
-        </div>
-        <button>Edit</button>
-        <button>Delete</button>
+
+      <div style={{ display: "flex", justifyContent: "center", padding: "2rem"}}>
+        <InvoiceToPDF invoice={demoInvoice} />
       </div>
-    </main>
+
+    </div>
   );
 };
 
+// export invoice page 
 export default Invoice;
