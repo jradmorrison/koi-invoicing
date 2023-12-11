c243ca3d5d7ec661c41e91c0428fe05fa3a7fd6e
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-12-11 10:11:05 -0500

    Removed Unused Database YAGNI

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,16 +1,10 @@
 // import type defs 
 const businessTypeDefs= require('./typedefs/businessTypeDefs');
-const serviceTypeDefs= require('./typedefs/serviceTypeDefs');
-const clientTypeDefs= require('./typedefs/clientTypeDefs');
-const contractTypeDefs= require('./typedefs/contractTypeDefs');
 const invoiceTypeDefs= require('./typedefs/invoiceTypeDefs');
 
 // export typedefs
 module.exports = `
     ${businessTypeDefs}
-    ${serviceTypeDefs}
-    ${clientTypeDefs}
-    ${contractTypeDefs}
     ${invoiceTypeDefs}
     
     type Auth {
@@ -22,16 +16,6 @@ module.exports = `
         currentBusiness: Business
         getBusinessByID(ID : ID!): Business!
         
-        getServiceByID(ID : ID!): Service!
-        getServicesByBusiness(businessId : ID!): [Service]
-        
-        getClientByID(ID: ID!): Client!
-        getClientsByBusiness(businessId: ID!): [Client]
-        
-        getContractByID(ID : ID!): Contract!
-        getContractsByBusiness(businessId : ID!): [Contract]
-        getContractsByClient(clientId : ID!): [Contract]
-        
         getInvoiceByID(ID: ID!): Invoice
         getInvoiceByBusiness(businessId: ID!): [Invoice]
     }
@@ -42,18 +26,6 @@ module.exports = `
         deleteBusiness(ID : ID!): Business!
         
         loginBusiness(email: String!, password: String!): Auth
-        
-        createService(serviceInput: ServiceInput!): Service!
-        updateService(ID : ID!, serviceUpdate: ServiceUpdate!): Service!
-        deleteService(ID : ID!): Service!
-        
-        createClient(contractID: ID, clientInput: ClientInput!): Client!
-        updateClient(clientUpdate: ClientUpdate!): Client!
-        deleteClient(ID : ID!): Client!
-        
-        createContract(contractInput: ContractInput): Contract!
-        updateContract(contractUpdate: ContractUpdate): Contract!
-        deleteContract(ID : ID!): Contract!
         
         createInvoice(invoiceInput: InvoiceInput): Invoice!
         updateInvoice(invoiceUpdate: InvoiceUpdate): Invoice!
