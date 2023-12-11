677c6881b44a057da028023e01a1176894521d5e
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-12-11 12:32:09 -0500

    fixed bug Dashboard Page and Invoices - Fixed Formated Dates on Invoices - Added Service Title and Client Name to cards

--- server/graphql/typedefs/invoiceTypeDefs.js
+++ server/graphql/typedefs/invoiceTypeDefs.js
@@ -2,6 +2,8 @@ module.exports = `
     type Invoice {
         _id: ID
         businessId: ID
+        serviceTitle: String
+        clientName: String
         clientEmail: String
         totalBalance: Float
         status: String
@@ -11,6 +13,8 @@ module.exports = `
 
     input InvoiceInput {
         businessId: ID
+        serviceTitle: String
+        clientName: String
         clientEmail: String
         totalBalance: Float
         status: String
