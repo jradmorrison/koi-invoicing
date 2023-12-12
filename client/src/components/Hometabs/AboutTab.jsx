0de9739caeb23febce103cd8950cacd2b332614f
Aprecity <kathrynfisher3700@gmail.com>
2023-12-12 10:59:16 -0500

    added screenshots

--- client/src/components/Hometabs/AboutTab.jsx
+++ client/src/components/Hometabs/AboutTab.jsx
@@ -1,8 +1,26 @@
+//import photos
+import mobile from "../../assets/images/mobileScreenshot.png";
+import web from "../../assets/images/invoiceScreenshot.png";
+
 export default function AboutTab() {
   return (
-    <div className="d-flex justify-content-center">
-    <p className="w-50 p-3">Koi is a comprehensive invoicing platform designed to streamline the billing process for freelancers and small businesses. <br/>With a powerful MERN stack foundation, we provide a user-friendly interface to create, manage, and send invoices effortlessly. Additionally, our platform allows you to set up subscription plans for recurring billing, making it easy to manage client payments. Stay organized and save time with Koi as you focus on growing your business. Coming soon!
-   </p>
+    <div className="holder">
+    <div className="">
+          <p className="p-3 about">
+            Koi is a comprehensive invoicing platform designed to streamline the
+            billing process for freelancers and small businesses.
+          </p>
+          <img src={web} className="float-right web"></img>
+        <img src={mobile} className='d-flex mobile'></img>
+          <p className="about">
+            With a powerful MERN stack foundation, we provide a user-friendly
+            interface to create, manage, and send invoices effortlessly.
+            Additionally, our platform allows you to set up subscription plans
+            for recurring billing, making it easy to manage client payments.
+            Stay organized and save time with Koi as you focus on growing your
+            business.
+          </p>
+    </div>
     </div>
   );
 }
