81b8907fbd1b10e7d4d1535e2dd320cf567ebff0
Aprecity <kathrynfisher3700@gmail.com>
2023-12-12 11:26:18 -0500

    fixed spacing on about page

--- client/src/components/Hometabs/AboutTab.jsx
+++ client/src/components/Hometabs/AboutTab.jsx
@@ -5,22 +5,32 @@ import web from "../../assets/images/invoiceScreenshot.png";
 export default function AboutTab() {
   return (
     <div className="holder">
-    <div className="">
-          <p className="p-3 about">
-            Koi is a comprehensive invoicing platform designed to streamline the
-            billing process for freelancers and small businesses.
+      <div className="d-flex justify-content-center gap-3 a2">
+        <div>
+          <p className="p-3 about text-left a1">
+            KOI is a comprehensive invoicing 
+            <br/>platform designed to streamline the billing process for 
+            <br/>freelancers and small businesses.
           </p>
-          <img src={web} className="float-right web"></img>
-        <img src={mobile} className='d-flex mobile'></img>
-          <p className="about">
-            With a powerful MERN stack foundation, we provide a user-friendly
-            interface to create, manage, and send invoices effortlessly.
-            Additionally, our platform allows you to set up subscription plans
-            for recurring billing, making it easy to manage client payments.
-            Stay organized and save time with Koi as you focus on growing your
+        </div>
+        <div>
+          <img src={web} className="web"></img>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        <div>
+        <img src={mobile} className="d-flex mobile"></img>
+        </div>
+        <div>
+        <p className="about p-3 text-right a3">
+            With a powerful MERN stack foundation, we provide a user-friendly <br/>
+            interface to create, manage, and send invoices effortlessly. <br/> <br/>
+
+            Stay organized and save time with KOI as you focus on growing your
             business.
           </p>
-    </div>
+        </div>
+      </div>
     </div>
   );
 }
