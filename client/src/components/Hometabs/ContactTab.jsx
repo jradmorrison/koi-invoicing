7cef106f2122c45fc94678f864e98abdbf5fb07c
Aprecity <kathrynfisher3700@gmail.com>
2023-12-12 11:55:46 -0500

    updated home tabs

--- client/src/components/Hometabs/ContactTab.jsx
+++ client/src/components/Hometabs/ContactTab.jsx
@@ -1,18 +1,18 @@
 export default function ContactTab() {
   return (
-    <div className="justify-content-center">
-      <div className="container p-4">
+    <div className="justify-content-center bg-light container">
+      <div className=" text-center p-4">
         <h2>Join the best invoicing platform today!</h2>
       </div>
-      <div className="container mb-4">
+      <div className="text-center mb-4">
         <button type="button" class="btn btn-warning">
           Click Here to Sign Up
         </button>
       </div>
-      <div className="container p-4">
+      <div className="text-center p-4">
         <h3>Already a member?</h3>
       </div>
-      <div className="container">
+      <div className="text-center">
         <button type="button" class="btn btn-secondary ">
           Login
         </button>
