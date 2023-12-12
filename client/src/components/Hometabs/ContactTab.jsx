ea312aed56c3e2751dc3e2819ed351019db59be0
Aprecity <kathrynfisher3700@gmail.com>
2023-12-12 12:04:42 -0500

    updated links on signup/login tab

--- client/src/components/Hometabs/ContactTab.jsx
+++ client/src/components/Hometabs/ContactTab.jsx
@@ -1,3 +1,5 @@
+import { Navigate, Link } from 'react-router-dom';
+
 export default function ContactTab() {
   return (
     <div className="justify-content-center bg-light container">
@@ -5,16 +7,14 @@ export default function ContactTab() {
         <h2>Join the best invoicing platform today!</h2>
       </div>
       <div className="text-center mb-4">
-        <button type="button" class="btn btn-warning">
-          Click Here to Sign Up
+        <button type="button" class="btn btn-secondary"><Link to={'/signup'} className='link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'>Click Here to Sign Up</Link>
         </button>
       </div>
       <div className="text-center p-4">
         <h3>Already a member?</h3>
       </div>
       <div className="text-center">
-        <button type="button" class="btn btn-secondary ">
-          Login
+        <button type="button" class="btn btn-secondary"><Link to={'/login'} className='link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover'>Proceed to Login</Link>
         </button>
       </div>
     </div>
