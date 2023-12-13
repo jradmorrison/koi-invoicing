9fb58a82bb86dfc9fbee4784dcf7e4534e6dd073
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-12-13 13:39:15 -0500

    updated deployed link on README

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +12,8 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
-  resolve: {alias: [{find: '@assets', replacement: '/src/assets'}]}
-})
+  resolve: { alias: [{ find: '@assets', replacement: '/src/assets' }] },
+});
