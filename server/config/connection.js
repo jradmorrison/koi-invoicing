e84f02f035d7977ca703c2f202017e0209c67b4e
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-12-13 12:17:52 -0500

    updated connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 // mongo cloud db 
-const MONGODB = process.env.MONGODB;
+const MONGODB_URI = process.env.MONGODB_URI;
 // connect to db
-mongoose.connect(MONGODB);
+mongoose.connect(process.env.MONGODB_URI);
 
 // export connection
 module.exports = mongoose.connection;
